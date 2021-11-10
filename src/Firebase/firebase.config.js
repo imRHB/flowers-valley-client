111ba495602ef3a79bc5aa837dbcfe634bde8df1
imprantu <rakinhasanbabu4@gmail.com>
2021-11-10 06:07:51 +0600

    initial commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBxHnHlu5X75P7H_W0HHGnxQNvImuzL28Y",
+    authDomain: "flowers-valley-shop.firebaseapp.com",
+    projectId: "flowers-valley-shop",
+    storageBucket: "flowers-valley-shop.appspot.com",
+    messagingSenderId: "1045707260323",
+    appId: "1:1045707260323:web:5406f3089ee6831b0c5a16"
+};
+
+export default firebaseConfig;
---
+++ src/Pages/Landing/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container my-5">
+            <h1>Welcome from Flowers Valley</h1>
+            <h4>React Bootstrap and React Router DOM</h4>
+        </div>
+    );
+};
+
+export default Home;
